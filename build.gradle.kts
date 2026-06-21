0d38a614e8f2b86f56b51f9b91ac8e69df579e24
Jonathan Zollinger <jonathan.zollinger@gmail.com>
2026-06-20 20:47:57 -0600

    ignore .env file

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,21 +5,29 @@ plugins {
     id("com.gradleup.shadow") version "8.3.3" apply false
     id("io.micronaut.aot") version "4.4.2" apply false
     id("gg.jte.gradle") version "3.1.12" apply false
+    id("org.sonarqube") version "latest.release"
+    id("jacoco")
 }
 
 allprojects {
     group = "lol.pbu.kaiju"
-    version = "0.1"
+    version = project.properties["kaijuVersion"]!!
 
     repositories {
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
-subprojects {
-    apply(plugin = "java")
-    java {
-        sourceCompatibility = JavaVersion.toVersion("21")
-        targetCompatibility = JavaVersion.toVersion("21")
+sonarqube {
+    properties {
+        property("sonar.tests", "src/test/groovy")
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
     }
 }
