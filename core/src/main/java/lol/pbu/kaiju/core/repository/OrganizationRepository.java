a1366a516dccf4e5bdb12ef6b0dd3ea5b1357aad
jonathan zollinger <jonathan.zollinger@outlook.com>
2026-06-22 22:06:13 -0600

    add basic org repo as a pageable CRUD repo

---
+++ core/src/main/java/lol/pbu/kaiju/core/repository/OrganizationRepository.java
@@ -0,0 +1,12 @@
+package lol.pbu.kaiju.core.repository;
+
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
+import io.micronaut.data.repository.PageableRepository;
+import lol.pbu.kaiju.core.domain.Organization;
+
+import java.util.UUID;
+
+@JdbcRepository(dialect = Dialect.POSTGRES)
+public interface OrganizationRepository extends PageableRepository<Organization, UUID> {
+}
