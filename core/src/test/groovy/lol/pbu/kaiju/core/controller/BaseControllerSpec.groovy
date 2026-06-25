de0c1e8278be376b814d78057a93a7c751a112a1
jonathan zollinger <jonathan.zollinger@outlook.com>
2026-06-24 21:27:55 -0600

    initial stab at validating schema allows for querying projects by location

---
+++ core/src/test/groovy/lol/pbu/kaiju/core/controller/BaseControllerSpec.groovy
@@ -0,0 +1,46 @@
+package lol.pbu.kaiju.core.controller
+
+import groovy.sql.Sql
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import spock.lang.Shared
+import spock.lang.Specification
+
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Method
+import java.lang.reflect.Proxy
+import java.sql.Connection
+import java.sql.DriverManager
+
+@MicronautTest
+class BaseControllerSpec extends Specification {
+
+    @Inject
+    @Shared
+    Connection connection
+
+    @Shared
+    Connection standaloneConnection
+
+    @Shared
+    Sql sql
+
+    def setupSpec() {
+        standaloneConnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/volunteer_monster", "jimmy", "warm-farts-smell-worse")
+        sql = new Sql((Connection) Proxy.newProxyInstance(
+                Connection.class.classLoader,
+                [Connection.class] as Class[],
+                { Object proxy, Method method, Object[] args ->
+                    try {
+                        return method.invoke(connection, args)
+                    } catch (Throwable ignored) {
+                        return method.invoke(standaloneConnection, args)
+                    }
+                } as InvocationHandler
+        ))
+    }
+
+    def cleanupSpec() {
+        standaloneConnection?.close()
+    }
+}
