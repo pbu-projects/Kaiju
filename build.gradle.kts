fb1d8ee9c9c10849a1c45062258ed99e95bd1830
jonathan zollinger <jonathan.zollinger@outlook.com>
2026-06-27 16:52:28 -0600

    add sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.gradleup.shadow") version "8.3.3" apply false
     id("io.micronaut.aot") version "4.4.2" apply false
     id("gg.jte.gradle") version "3.1.12" apply false
+    id("org.sonarqube") version "7.3.1.8318"
 }
 
 allprojects {
@@ -23,3 +24,17 @@ subprojects {
         targetCompatibility = JavaVersion.toVersion("21")
     }
 }
+
+sonar {
+    properties {
+        property("sonar.projectKey", "pbu-projects_Kaiju")
+        property("sonar.organization", "peanutbutter-unicorn")
+        property("sonar.host.url", "https://sonarcloud.io")
+        (System.getenv("SONAR_TOKEN") ?: System.getenv("sonar_token"))?.let { token ->
+            if (token.isNotBlank()) {
+                property("sonar.token", token)
+            }
+        }
+    }
+}
+
